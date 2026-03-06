a1a69c035de8a41b44be1c010c7d04db145d41d0
Aliyan Sajid <aliyansajid127@gmail.com>
2026-03-06 21:25:20 +0500

    Admin app setup

---
+++ apps/admin/app/(auth)/login/page.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import { LoginForm } from "@/components/login-form";
+import Link from "next/link";
+
+export default function LoginPage() {
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <Link href="/" className="flex self-center">
+          <Image src="/logo.svg" alt="Logo" width={24} height={24} />
+        </Link>
+        <LoginForm />
+      </div>
+    </div>
+  );
+}
