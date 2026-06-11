54f2fdf9c8b13d03a5f93951e191c914c5c64fa9
Aliyan Sajid <aliyansajid127@gmail.com>
2026-06-11 14:23:16 +0500

    security: remove injected malicious code from config files

--- apps/auth/next.config.js
+++ apps/auth/next.config.js
@@ -1,7 +1,8 @@
-import { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
-const nextConfig: NextConfig = {};
-
 const withNextIntl = createNextIntlPlugin();
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
 export default withNextIntl(nextConfig);
--- apps/seller/next.config.js
+++ apps/seller/next.config.js
@@ -1,7 +1,9 @@
-import { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
-const nextConfig: NextConfig = {
+const withNextIntl = createNextIntlPlugin();
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
     remotePatterns: [
       {
@@ -20,5 +22,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-const withNextIntl = createNextIntlPlugin();
 export default withNextIntl(nextConfig);
