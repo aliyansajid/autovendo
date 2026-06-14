cfd72eb96abef918b146ebd1f03492c448a412f4
Aliyan Sajid <aliyansajid127@gmail.com>
2026-06-14 12:09:09 +0500

    fix: use lowercase vehicle table name in migration sql

--- packages/database/prisma/migrations/20260606000000_add_vehicle_description_length_constraint/migration.sql
+++ packages/database/prisma/migrations/20260606000000_add_vehicle_description_length_constraint/migration.sql
@@ -1,3 +1,3 @@
 -- Add CHECK constraint: vehicleDescription must be <= 1000 characters when not NULL
-ALTER TABLE "Vehicle" ADD CONSTRAINT "chk_vehicle_description"
+ALTER TABLE "vehicle" ADD CONSTRAINT "chk_vehicle_description"
   CHECK ("vehicleDescription" IS NULL OR length("vehicleDescription") <= 1000);
