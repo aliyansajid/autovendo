e1d50e033970a4f73db284907d735040da30e491
Aliyan Sajid <aliyansajid127@gmail.com>
2026-06-15 13:41:32 +0500

    use R2_ENDPOINT env var to support EU region bucket

--- apps/api/src/upload/upload.controller.ts
+++ apps/api/src/upload/upload.controller.ts
@@ -8,7 +8,9 @@ import multer from "multer";
 
 const s3 = new S3Client({
   region: "auto",
-  endpoint: `https://${process.env.R2_ACCOUNT_ID}.r2.cloudflarestorage.com`,
+  endpoint:
+    process.env.R2_ENDPOINT ??
+    `https://${process.env.R2_ACCOUNT_ID}.r2.cloudflarestorage.com`,
   credentials: {
     accessKeyId: process.env.R2_ACCESS_KEY_ID!,
     secretAccessKey: process.env.R2_SECRET_ACCESS_KEY!,
